eff5562e8099beb26ce30d2c9a925379e916e184
zongweif <63831566+zongweif@users.noreply.github.com>
2020-07-15 12:48:24 -0700

    user authentication

--- app/src/main/java/neu/edu/crease/LoginActivity.java
+++ app/src/main/java/neu/edu/crease/LoginActivity.java
@@ -1,14 +1,112 @@
 package neu.edu.crease;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText password_register, email_register;
+    private Button login;
+    private TextView signup_text;
+
+    private FirebaseAuth auth;
+
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // init fields
+        email_register = findViewById(R.id.email_register);
+        password_register = findViewById(R.id.password_register);
+        login = findViewById(R.id.login_button);
+        signup_text = findViewById(R.id.signup_text);
+
+        auth = FirebaseAuth.getInstance();
+
+        // if user click the register button, just direct to register page
+        signup_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        // if user click the login button
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // show the waiting message
+                pd = new ProgressDialog(LoginActivity.this);
+                pd.setMessage("Please wait...");
+                pd.show();
+
+                // get the info from user typing
+                String str_email = email_register.getText().toString();
+                String str_password = password_register.getText().toString();
+
+                // if some info empty
+                if (TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)) {
+                    Toast.makeText(LoginActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // let firebase auth sign in the user
+                    auth.signInWithEmailAndPassword(str_email, str_password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // create the user for firebase database
+                                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users")
+                                                .child(auth.getCurrentUser().getUid());
+
+                                        // redirect to start activity
+                                        reference.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                pd.dismiss();
+                                                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                pd.dismiss();
+                                            }
+                                        });
+                                    }
+                                    else {
+                                        pd.dismiss();
+                                        Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
