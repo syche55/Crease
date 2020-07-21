a2ffc53695e41ccb3b4026d3ad77840e2bd49ac1
zongweif <63831566+zongweif@users.noreply.github.com>
2020-07-20 17:51:38 -0700

    userProfile

--- app/src/main/java/neu/edu/crease/ui/profile/ProfileFragment.java
+++ app/src/main/java/neu/edu/crease/ui/profile/ProfileFragment.java
@@ -1,9 +1,14 @@
 package neu.edu.crease.ui.profile;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +17,59 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import neu.edu.crease.R;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
+
+
+    ImageView image_profile, options;
+    TextView posts, followers, following, username;
+    Button edit_profile;
+
+    FirebaseUser firebaseUser;
+    String profileid;
+
+    ImageButton my_fotos, saved_fotos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        SharedPreferences prefs = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        profileid = prefs.getString("profileid", "none");
+
+        image_profile = view.findViewById(R.id.image_profile);
+        options = view.findViewById(R.id.options);
+        posts = view.findViewById(R.id.posts);
+        followers = view.findViewById(R.id.followers);
+        following = view.findViewById(R.id.following);
+        username = view.findViewById(R.id.username);
+        edit_profile = view.findViewById(R.id.edit_profile);
+        my_fotos = view.findViewById(R.id.my_fotos);
+        saved_fotos = view.findViewById(R.id.saved_fotos);
+
+
+        edit_profile.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                String btn = edit_profile.getText().toString();
+
+                if (btn.equals("Edit Profile")) {
+
+                }
+                else if (btn.equals("follow")) {
+
+                }
             }
         });
-        return root;
+
+
+        return view;
     }
 }
